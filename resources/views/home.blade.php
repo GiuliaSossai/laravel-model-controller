54ae537c50c3fe69eff9cb856de03afb02f99762
GiuliaSossai <giulia.sossai@yahoo.it>
2022-01-24 16:48:39 +0100

    card film

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
     <div class="container">
-        <h1>movie home</h1>
+        <h1>Home</h1>
     </div>
 
-    <div class="container">
-        <table class="table">
+    <main class="container">
+        <!-- <table class="table">
             <thead>
                 <tr>
                     <th scope="col">#id</th>
@@ -22,7 +22,7 @@
 
             @foreach ($movies as $movie)
                 <tr>
-                    <th scope="row">1</th>
+                    <th scope="row">{{ $movie->id}}</th>
                     <td>{{ $movie->title}}</td>
                     <td>{{ $movie->original_title}}</td>
                     <td>{{ $movie->nationality}}</td>
@@ -33,7 +33,21 @@
                 
                 
             </tbody>
-        </table>
-    </div>
+        </table> -->
+
+
+        <div class="movie-row row justify-content-between">
+        @foreach ($movies as $movie)
+                <div class="movie-box col-3 m-3 py-2">
+                    <h2>{{ $movie->title}}</h2>
+                    <p>Titolo originale: <strong>{{ $movie->original_title }}</strong> </p>
+                    <p>Paese: <strong>{{ $movie->nationality }}</strong> </p>
+                    <p>Data di uscita: <strong>{{ $movie->date }}</strong> </p>
+                    <p>Voto: <strong>{{ $movie->vote }}</strong> </p>
+
+                </div>
+            @endforeach
+        </div>
+    </main>
     
 @endsection
