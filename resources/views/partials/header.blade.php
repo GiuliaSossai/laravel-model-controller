f8f97940497b0f8ba7b6918000157148d6835194
GiuliaSossai <giulia.sossai@yahoo.it>
2022-01-24 15:40:30 +0100

    init project

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Movie</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contacts</a>
+        </li>
+    </ul>
+</header>
